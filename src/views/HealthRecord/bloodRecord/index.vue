30d8eb5e47d1cee1feb5ae714eb04b7df14c8756
Yasin0324 <119337341+Yasin0324@users.noreply.github.com>
2024-04-08 20:47:17 +0800

    feat:血糖记录页面完善

--- src/views/HealthRecord/bloodRecord/index.vue
+++ src/views/HealthRecord/bloodRecord/index.vue
@@ -8,15 +8,22 @@
                         v-model="recordDate"
                         type="date"
                         placeholder="选 择 日 期"
+                        format="YYYY-MM-DD"
+                        value-format="YYYY-MM-DD"
                         :default-active="new Date()"
                     ></el-date-picker>
                 </div>
             </div>
 
             <div class="recordCard">
-                <div class="card" v-for="item in 7">
-                    <div class="cardContent">7.2</div>
-                    <div class="cardFooter">早餐前</div>
+                <div class="card" v-for="item in 1">
+                    <div class="cardMain">
+                        <div class="cardContent">
+                            <h1>7.2</h1>
+                            <p>mmol/L</p>
+                        </div>
+                        <div class="cardFooter">早餐前</div>
+                    </div>
                 </div>
                 <div class="card">
                     <div class="recordInfo" v-show="recordOpen">
@@ -86,7 +93,10 @@
                                         v-model="remark"
                                         type="textarea"
                                         placeholder="添加备注"
+                                        :rows="4"
+                                        :autosize="{ minRows: 4, maxRows: 4 }"
                                         style="margin-top: 0.5vh"
+                                        resize="none"
                                     ></el-input>
                                 </el-form-item>
                             </el-form>
@@ -115,11 +125,49 @@
                 </div>
             </div>
         </div>
+        <div class="bloodVisible">
+            <div class="visibleHeader">
+                <div class="title"><h1>血糖可视化</h1></div>
+                <div class="datePicker">
+                    <el-date-picker
+                        v-model="visibleDate"
+                        type="daterange"
+                        range-separator="到"
+                        start-placeholder="开始日期"
+                        end-placeholder="结束日期"
+                        :default-active="new Date()"
+                        format="YYYY-MM-DD"
+                        value-format="YYYY-MM-DD"
+                        :disabled-date="(time) => time.getTime() > Date.now()"
+                    ></el-date-picker>
+                </div>
+            </div>
+            <div class="visibleContent">
+                <div
+                    id="bloodVisibleChart"
+                    style="width: 100%; height: 50vh"
+                ></div>
+            </div>
+        </div>
+        <div class="bloodAnalyse">
+            <div class="analyseHeader">
+                <div class="title"><h1>血糖分析</h1></div>
+                <div class="datePicker">
+                    <el-radio-group v-model="analyseDate">
+                        <el-radio value="recent7days">近7天</el-radio>
+                        <el-radio value="recent15days">近15天</el-radio>
+                        <el-radio value="recent30days">近30天</el-radio>
+                    </el-radio-group>
+                </div>
+            </div>
+            <div class="analyseMain" style="height: 50vh"></div>
+        </div>
     </div>
 </template>
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { setBloodRecord } from "../../../api/healthRecord";
+import * as echarts from "echarts";
 // 血糖记录日期
 const recordDate = ref(new Date());
 // 添加记录
@@ -141,6 +189,7 @@ const options = ref([
 // 记录备注
 const remark = ref("");
 // 日期格式化
+// YYYY-MM-DD HH:mm:ss
 function formatDateTime(date) {
     let yyyy = date.getFullYear();
     let MM = String(date.getMonth() + 1).padStart(2, "0"); // 月份是从0开始的
@@ -149,6 +198,13 @@ function formatDateTime(date) {
     let mm = String(date.getMinutes()).padStart(2, "0");
     let ss = String(date.getSeconds()).padStart(2, "0");
     return `${yyyy}-${MM}-${dd} ${HH}:${mm}:${ss}`;
+}
+// YYYY-MM-DD
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const day = date.getDate().toString().padStart(2, "0");
+    return `${year}-${month}-${day}`;
 }
 // 添加记录
 const addRecord = () => {
@@ -167,14 +223,122 @@ const addRecord = () => {
             console.log(err);
         });
 };
+// 可视化日期记录
+const visibleDate = ref([
+    formatDate(new Date(new Date().setDate(new Date().getDate() - 7))),
+    formatDate(new Date()),
+]);
+// 血糖记录可视化
+function showBloodVisible() {
+    const chartDom = document.getElementById("bloodVisibleChart");
+    chartDom?.removeAttribute("_echarts_instance_");
+    const myChart = echarts.init(chartDom);
+
+    const data = [
+        ["2000-06-05", 116],
+        ["2000-06-06", 129],
+        ["2000-06-07", 135],
+        ["2000-06-08", 86],
+        ["2000-06-09", 73],
+        ["2000-06-10", 85],
+        ["2000-06-11", 73],
+        ["2000-06-12", 68],
+        ["2000-06-13", 92],
+        ["2000-06-14", 130],
+        ["2000-06-15", 245],
+        ["2000-06-16", 139],
+        ["2000-06-17", 115],
+        ["2000-06-18", 111],
+        ["2000-06-19", 309],
+        ["2000-06-20", 206],
+        ["2000-06-21", 137],
+        ["2000-06-22", 128],
+        ["2000-06-23", 85],
+        ["2000-06-24", 94],
+        ["2000-06-25", 71],
+        ["2000-06-26", 106],
+        ["2000-06-27", 84],
+        ["2000-06-28", 93],
+        ["2000-06-29", 85],
+        ["2000-06-30", 73],
+        ["2000-07-01", 83],
+        ["2000-07-02", 125],
+        ["2000-07-03", 107],
+        ["2000-07-04", 82],
+        ["2000-07-05", 44],
+        ["2000-07-06", 72],
+        ["2000-07-07", 106],
+        ["2000-07-08", 107],
+        ["2000-07-09", 66],
+        ["2000-07-10", 91],
+        ["2000-07-11", 92],
+        ["2000-07-12", 113],
+        ["2000-07-13", 107],
+        ["2000-07-14", 131],
+        ["2000-07-15", 111],
+        ["2000-07-16", 64],
+        ["2000-07-17", 69],
+        ["2000-07-18", 88],
+        ["2000-07-19", 77],
+        ["2000-07-20", 83],
+        ["2000-07-21", 111],
+        ["2000-07-22", 57],
+        ["2000-07-23", 55],
+        ["2000-07-24", 60],
+    ];
+    const dateList = data.map(function (item) {
+        return item[0];
+    });
+    const valueList = data.map(function (item) {
+        return item[1];
+    });
+    const option = {
+        visualMap: [
+            {
+                show: false,
+                type: "continuous",
+                seriesIndex: 0,
+                min: 0,
+                max: 400,
+            },
+        ],
+        title: [
+            {
+                left: "center",
+                text: "Gradient along the y axis",
+            },
+        ],
+        tooltip: {
+            trigger: "axis",
+        },
+        xAxis: [
+            {
+                data: dateList,
+            },
+        ],
+        yAxis: [{}],
+        series: [
+            {
+                type: "line",
+                showSymbol: false,
+                data: valueList,
+            },
+        ],
+    };
+    myChart.setOption(option);
+}
+// 血糖分析日期
+const analyseDate = ref("recent7days");
+onMounted(() => {
+    showBloodVisible();
+});
 </script>
 <style lang="less" scoped>
 .main {
     margin-top: 2vh;
     .record {
         width: 130vh;
-        height: 80vh;
-        margin: 0 auto;
+        margin: 0 auto 2vh;
         border-radius: 5vh;
         background-color: #ecf0f3;
         box-shadow: 1vh 1vh 1vh #d1d9e6;
@@ -201,22 +365,39 @@ const addRecord = () => {
             flex-wrap: wrap;
             justify-content: start;
             .card {
-                margin: 3vh 1vh 1vh 5vh;
+                margin: 3vh 1vh 2vh 5vh;
                 background-color: #fff;
                 width: 25vh;
                 height: 30vh;
                 border-radius: 5vh;
-                .cardContent {
-                    margin-top: 5vh;
-                    height: 5vh;
-                    line-height: 5vh;
-                    font-size: 4.8vh;
-                    font-weight: bold;
-                    color: #009688;
-                }
-                .cardFooter {
-                    margin-top: 2vh;
-                    font-size: 2.5vh;
+                .cardMain {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    .cardContent {
+                        margin-top: 5vh;
+                        display: flex;
+                        flex-direction: row;
+                        align-items: end;
+                        h1 {
+                            height: 5vh;
+                            line-height: 5vh;
+                            font-size: 4.8vh;
+                            font-weight: bold;
+                            color: #009688;
+                        }
+                        p {
+                            margin-left: 1vh;
+                            height: 5vh;
+                            line-height: 5vh;
+                            font-size: 2vh;
+                            font-weight: bold;
+                        }
+                    }
+                    .cardFooter {
+                        margin-top: 2vh;
+                        font-size: 2.5vh;
+                    }
                 }
                 .buttonAll {
                     cursor: pointer;
@@ -238,7 +419,7 @@ const addRecord = () => {
                     }
                     :deep(.el-button) {
                         border: none;
-                        background-color: rgba(163, 197, 125, 0.7);
+                        background-color: rgba(0, 150, 136, 0.7);
                     }
                     .buttonText {
                         font-size: 2.5vh;
@@ -273,6 +454,54 @@ const addRecord = () => {
                             font-weight: bold;
                         }
                     }
+                }
+            }
+        }
+    }
+    .bloodVisible {
+        width: 130vh;
+        margin: 0 auto 2vh;
+        border-radius: 5vh;
+        background-color: #ecf0f3;
+        box-shadow: 1vh 1vh 1vh #d1d9e6;
+        .visibleHeader {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            border-bottom: 1vh solid #a0a5a8;
+            padding-left: 3vh;
+            .title {
+                color: #01111abe;
+            }
+            :deep(.datePicker) {
+                margin-left: 3vh;
+                margin-top: 1vh;
+                .el-date-editor {
+                    width: 25vh;
+                }
+            }
+        }
+    }
+    .bloodAnalyse {
+        width: 130vh;
+        margin: 0 auto 2vh;
+        border-radius: 5vh;
+        background-color: #ecf0f3;
+        box-shadow: 1vh 1vh 1vh #d1d9e6;
+        .analyseHeader {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            border-bottom: 1vh solid #a0a5a8;
+            padding-left: 3vh;
+            .title {
+                color: #01111abe;
+            }
+            :deep(.datePicker) {
+                margin-left: 0vh;
+                margin-top: 1vh;
+                .el-radio-group {
+                    margin-left: 3vh;
                 }
             }
         }
