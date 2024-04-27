f1f1c3167ab0237c1e59e23f8709a6b01576f4c3
江煜铭 <jiangyuming@jiangyumingdeMacBook-Air.local>
2024-04-27 17:17:21 +0800

    公告管理样式修改

--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -57,17 +57,6 @@
                     </span>
                 </div>
             </el-backtop>
-            <el-backtop
-                :right="100"
-                style="
-                    background-color: var(--el-bg-color-overlay);
-                    box-shadow: var(--el-box-shadow-lighter);
-                    text-align: center;
-                    line-height: 40px;
-                    color: #a3c576;
-                "
-                :bottom="100"
-            />
         </div>
     </div>
 </template>
